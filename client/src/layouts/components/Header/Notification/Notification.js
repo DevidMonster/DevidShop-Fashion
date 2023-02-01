80467332f0baccb36b89be74b21401be3f954263
DevidMonster <mrbat905@gmail.com>
2023-02-01 23:46:25 +0700

    fix responsive home page and update detail page

--- client/src/layouts/components/Header/Notification/Notification.js
+++ client/src/layouts/components/Header/Notification/Notification.js
@@ -4,18 +4,29 @@ import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { useSelector } from "react-redux";
 import { Wrapper } from "../../../../components/popper";
-import { FaBell } from '../../../../asset/icons';
+import { FaBell, BsBellSlash } from '../../../../asset/icons';
 import Button from "../../../../components/Button";
+import { useState } from 'react';
 
 const cx = classNames.bind(styles)
 
 function Notification() {
     let mode = useSelector(state => state.active) || false
     if(localStorage.getItem('mode'))  mode = localStorage.getItem('mode') === "true" ? true : false;
+    
+    const [data, setData] = useState([])
 
     const renderAlert = (attrs) => (
         <div className={cx('alert-list')} tabIndex="-1" {...attrs}>
             <Wrapper>
+                {data.length > 0 ? (
+                    <div></div>
+                ) : (
+                    <div className={cx('no_result')}>
+                        <BsBellSlash/>
+                        <p>No Notification</p>
+                    </div>
+                )}
             </Wrapper>
         </div>
     );
