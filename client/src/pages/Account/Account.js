62d4606d30073ce026a3163aefa6f60e00960ff4
DevidMonster <mrbat905@gmail.com>
2023-03-03 18:32:03 +0700

    Styling account page

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -3,44 +3,19 @@ import classNames from 'classnames/bind';
 
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useUploadImage } from '../../hooks';
-import Tippy from '@tippyjs/react/headless';
-import * as request from '../../utils/httpRequest';
 
 import { userSelector } from '../../redux/selectors';
-import images from '../../asset/images';
-import { AiFillEdit } from '../../asset/icons';
-import Button from '../../components/Button';
-import reducers from '../../redux/reducer';
+
+import Avatar from './Avatar/Avatar';
 
 const cx = classNames.bind(styles)
 
 function Account() {
     const getUser = useSelector(userSelector)
     const [user, setUser] = useState(getUser || {})
-    const [show, setShow] = useState(false)
-    const [newAvatar, setNewAvatar] = useState("")
 
-    let mode = useSelector(state => state.active) || false
-    if(localStorage.getItem('mode'))  mode = localStorage.getItem('mode') === "true" ? true : false;
     const dispatch = useDispatch()
 
-    const getNewImage = useUploadImage()
-
-    const uploadImage = async (e) => {
-        const newImage = await getNewImage(e.target.files)
-        setNewAvatar(newImage)
-    }
-
-    const saveImage = async () => {
-        const res = await request.put("/general/user/img", {
-            id: user._id,
-            avatar: newAvatar
-        })
-        dispatch(reducers.actions.currentUser(res))
-        setShow(false)
-        setNewAvatar("" )
-    }
 
     useEffect(() => {
         setUser(getUser || {})
@@ -48,34 +23,7 @@ function Account() {
 
     return (  
         <div className={cx('wrapper')}>
-            <div className={cx('header')}>   
-                <div className={cx('avatar')}>
-                    <img src={user.avatar || images.user} alt={user.name}/>
-                    <Tippy
-                        interactive
-                        theme={mode ? 'light' : 'material'}
-                        placement="bottom"
-                        render={(attrs) => (
-                            <Button className={cx('edit')} onClick={() => setShow(prev => !prev)}>Upload Image</Button>
-                        )}
-                    >
-                        <span>Edit Avatar <AiFillEdit/></span>
-                    </Tippy>
-                    {show && (
-                        <div className={cx('image_popup')}>
-                            <Button onClick={() => setShow(false)}>Close</Button>
-                            <input type="file" multiple onChange={e => uploadImage(e)}/>
-                            <img src={newAvatar}/>
-                            <Button className={cx('cancel')} onClick={() => setNewAvatar("")}>Cancel</Button>
-                            <Button className={cx('save')} onClick={() => saveImage()}>Save</Button>
-                        </div>
-                    )}
-                </div>
-                <div className={cx('name_info')}>
-                    <h1>{user.name}</h1>
-                    <p>{user.email}</p>
-                </div>
-            </div>
+            <Avatar user={user}/>
         </div>
     );
 }
---
+++ client/src/pages/Account/Avatar/Avatar.js
@@ -0,0 +1,88 @@
+import styles from './Avatar.module.scss';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import images from '../../../asset/images';
+import { AiFillEdit, IoCloseSharp } from '../../../asset/icons';
+import Button from '../../../components/Button';
+import reducers from '../../../redux/reducer';
+import Loading from '../../../components/Loading';
+import { useUploadImage } from '../../../hooks';
+import * as request from '../../../utils/httpRequest';
+
+const cx = classNames.bind(styles)
+
+function Avatar({ user }) {
+    const [show, setShow] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [newAvatar, setNewAvatar] = useState("")
+
+    const dispatch = useDispatch()
+
+    const getNewImage = useUploadImage()
+
+    const uploadImage = async (e) => {
+        setLoading(true)
+        const newImage = await getNewImage(e.target.files)
+        setLoading(false)
+        setNewAvatar(newImage)
+    }
+
+    const saveImage = async () => {
+        setLoading(true)
+        const res = await request.put("/general/user/img", {
+            id: user._id,
+            avatar: newAvatar
+        })
+        setLoading(false)
+        dispatch(reducers.actions.currentUser(res))
+        setShow(false)
+        setNewAvatar("")
+    }
+
+    return (
+        <div className={cx('header')}>
+            <div className={cx('avatar')}>
+                <img src={user.avatar || images.user} alt={user.name} />
+                <Button className={cx('edit')} small red rightIcon={<AiFillEdit />} onClick={() => setShow(prev => !prev)}>Edit Avatar</Button>
+                {show && (
+                    <div className={cx('image_popup')}>
+                        <div className={cx('top')}>
+                            <h1>Change Avatar</h1>
+                            <Button icon={<IoCloseSharp />} onClick={() => {
+                                setNewAvatar("")
+                                setShow(false)
+                            }} />
+                        </div>
+                        <div className={cx('mid')}>
+                            <div>
+                                <input type="file" name="file" id="file" className={cx("inputfile")} multiple onChange={e => uploadImage(e)} />
+                                <label htmlFor="file">Choose a file</label>
+                            </div>
+                            <div className={cx('showImage')}>
+                                {newAvatar !== "" && (
+                                    loading ? (
+                                        <Loading />
+                                    ) : (
+                                        <img src={newAvatar} />
+                                    )
+                                )}
+                            </div>
+                        </div>
+                        <div className={cx('bot')}>
+                            <Button className={cx('cancel')} onClick={() => setNewAvatar("")} disibled={newAvatar === "" ? true : false}>Cancel</Button>
+                            <Button className={cx('save')} onClick={() => saveImage()} disibled={newAvatar === "" ? true : false}>Save</Button>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className={cx('name_info')}>
+                <h1>{user.name}</h1>
+                <p>{user.email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Avatar;
