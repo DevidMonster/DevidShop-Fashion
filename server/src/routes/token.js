3eae2c65170ce891765055aa7d7cbb8874c30192
quanhay3 <quanhay23@gmail.com>
2024-11-14 14:42:11 +0700

    add token router

--- server/src/routes/token.js
+++ server/src/routes/token.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/callback', (req, res) => {
     try {
-        res.status(200).json(res)
+        res.status(200).json({code: req.query.code})
     } catch(err) {
         res.status(404).json({message: err.message})
     }
